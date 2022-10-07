05933329c72107553b5a00d292c69943d92bb54d
Simone Marcantel☆ <simonemarcantel04@gmail.com>
2022-10-07 10:14:44 -0500

    functions parameters

---
+++ main.js
@@ -0,0 +1,13 @@
+const quarterValue = (wholeNumber) => {
+    const quarter = wholeNumber / 4
+
+    return quarter
+    // Invoke the function and store the return value
+}
+const quarteredNumber = quarterValue(400)
+console.log(quarteredNumber)
+
+console.log(quarterValue(400))
+
+// Log the return value
+
